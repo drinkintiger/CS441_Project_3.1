a935bacf7a4df34c953a704964a8924884a1a9d7
DrinkinTiger <tsoy_vladimir@yahoo.com>
2012-11-12 20:31:44 -0600

    initial commit

---
+++ matrixmult.c
@@ -0,0 +1,16 @@
+/*
+ * <AUTHOR>
+ * <DATE>
+ *
+ * CS 441/541 : Project 3 Part 1
+ */
+#include "matrixmult.h"
+
+int main(int argc, char * argv[]) {
+
+    support_init();
+
+    support_finalize();
+
+    return 0;
+}
---
+++ matrixmult.h
@@ -0,0 +1,7 @@
+/*
+ * <AUTHOR>
+ * <DATE>
+ *
+ * CS 441/541 : Project 3 Part 1
+ */
+#include "support.h"
