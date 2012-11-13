ca5398185bcfa0353c41cfcdf36847eba043b3d8
Jordan <adsf>
2012-11-13 11:46:41 -0600

    i think this is all we need for scalarmult, added in MAX_ITERS as global var set to 1000

--- scalarmult.c
+++ scalarmult.c
@@ -7,11 +7,13 @@
 #include "scalarmult.h"
 
 double run_experiment_ij ( mtype_t *matrix , mtype_t scalar , int buffer_size );
-
+int MAX_ITERS = 1000;
 int main(int argc, char * argv[]) {
     mtype_t * matrix = NULL ;
     int i, MAX_SIZE;
     support_init();
+    
+    MAX_ITERS = 1000;
     
     if(argc==2)
         MAX_SIZE = atoi(argv[1]);
@@ -23,9 +25,9 @@ int main(int argc, char * argv[]) {
         clear_matrix (matrix , i );
         
         printf("------------------------------------------------------------------------------\n");
-        double avg_time = run_experiment_ij (matrix, random(), i);
+        double avg_time = run_experiment_ij (matrix, 5.0, i);
         printf("The average time to do scalar multiplication ij on a %d x %d matrix is %lf \n", i, i, avg_time);
-        avg_time = run_experiment_ij (matrix, random(), i);
+        avg_time = run_experiment_ij (matrix, 5.0, i);
         printf("The average time to do scalar multiplication ji on a %d x %d matrix is %lf \n", i, i, avg_time);
         
     }
@@ -41,7 +43,7 @@ double run_experiment_ij ( mtype_t *matrix , mtype_t scalar , int buffer_size ) 
     double final_time ;
     
     start = get_time ();
-    for ( iter = 0; iter < 5 ; ++ iter ) {
+    for ( iter = 0; iter < MAX_ITERS ; ++ iter ) {
         for (i = 0; i < buffer_size ; ++i ) {
             for (j = 0; j < buffer_size ; ++j ) {
                 matrix [ GET_INDEX (i, j, buffer_size ) ] = scalar * matrix [ GET_INDEX (i, j, buffer_size ) ];
@@ -49,7 +51,7 @@ double run_experiment_ij ( mtype_t *matrix , mtype_t scalar , int buffer_size ) 
         }
     }
     end = get_time ();
-    return final_time = diff_timers (start , end) / 5 ;
+    return final_time = diff_timers (start , end) / MAX_ITERS ;
 }
 double run_experiment_ji ( mtype_t * matrix , mtype_t scalar , int buffer_size ) {
     int i, j, iter;
@@ -57,7 +59,7 @@ double run_experiment_ji ( mtype_t * matrix , mtype_t scalar , int buffer_size )
     double final_time ;
     
     start = get_time ();
-    for ( iter = 0; iter < 5 ; ++ iter ) {
+    for ( iter = 0; iter < MAX_ITERS ; ++ iter ) {
         for (j = 0; j < buffer_size ; ++j ) {
             for (i = 0; i < buffer_size ; ++i ) {
                 matrix [ GET_INDEX (i, j, buffer_size ) ] = scalar * matrix [ GET_INDEX (i, j, buffer_size ) ];
@@ -65,5 +67,5 @@ double run_experiment_ji ( mtype_t * matrix , mtype_t scalar , int buffer_size )
         }
     }
     end = get_time ();
-    return final_time = diff_timers (start , end) / 5 ;
+    return final_time = diff_timers (start , end) / MAX_ITERS ;
 }
