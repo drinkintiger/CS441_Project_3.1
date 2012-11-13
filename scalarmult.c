2aa5b2e539660852f000e486fd0c9b3e7a7cdbfa
DrinkinTiger <tsoy_vladimir@yahoo.com>
2012-11-12 22:37:40 -0600

    changed buffer side = buffer size

--- scalarmult.c
+++ scalarmult.c
@@ -15,9 +15,6 @@ int main(int argc, char * argv[]) {
 
     support_finalize();
 
-    // Access matrix [i][j] in the 1D array
-    matrix [ GET_INDEX (i, j, buffer_side ) ] = ...;
-    
     for ( i = 2; i <= 1024; i * 2 ) {
         // Allocate a matrix of size : buffer_side x buffer_side
         allocate_matrix (& matrix , i );
@@ -34,7 +31,7 @@ double run_experiment_ij ( mtype_t *matrix , mtype_t scalar , int buffer_size ) 
     for ( iter = 0; iter < MAX_ITERS ; ++ iter ) {
         for (i = 0; i < buffer_size ; ++i ) {
             for (j = 0; j < buffer_size ; ++j ) {
-                matrix [i][j] = scalar * matrix [i][j];
+                matrix [ GET_INDEX (i, j, buffer_size ) ] = scalar * matrix [i][j];
             }
         }
     }
